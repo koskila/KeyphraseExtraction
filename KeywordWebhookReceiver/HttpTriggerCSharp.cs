a1daa13229c0fff7f0f8d1f3f166f28d365dc0eb
Antti K. Koskela <posti@koskila.net>
2017-12-14 12:16:00 +0200

    Updates readme and cleans code

--- KeywordWebhookReceiver/HttpTriggerCSharp.cs
+++ KeywordWebhookReceiver/HttpTriggerCSharp.cs
@@ -305,7 +305,6 @@ namespace KeywordWebhookReceiver
             }
         }
 
-
         private static void UpdateManagedMetadata(string[] value, TraceWriter log, ClientContext ctx, ListItem item, Field field)
         {
             try
@@ -375,32 +374,6 @@ namespace KeywordWebhookReceiver
             {
                 log.Error(ex.Message);
             }
-        }
-
-
-        public static void UpdateTaxonomyField(ClientContext ctx, List list, ListItem listItem, string fieldName, string fieldValue)
-        {
-            Field field = list.Fields.GetByInternalNameOrTitle(fieldName);
-            TaxonomyField txField = ctx.CastTo<TaxonomyField>(field);
-            TaxonomyFieldValue termValue = new TaxonomyFieldValue();
-            string[] term = fieldValue.Split('|');
-            termValue.Label = term[0];
-            termValue.TermGuid = term[1];
-            termValue.WssId = -1;
-            txField.SetFieldValueByValue(listItem, termValue);
-            listItem.Update();
-            ctx.Load(listItem);
-            ctx.ExecuteQuery();
-        }
-
-        public static void ClearTaxonomyFieldValue(ClientContext ctx, List list, ListItem listItem, string fieldName)
-        {
-            Field field = list.Fields.GetByInternalNameOrTitle(fieldName);
-            TaxonomyField txField = ctx.CastTo<TaxonomyField>(field);
-            txField.ValidateSetValue(listItem, null);
-            listItem.Update();
-            ctx.Load(listItem);
-            ctx.ExecuteQuery();
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Keyword and Tagging examples
+
+This is my public repository for testing all kinds of Cognitive Services -related demos etc.
+
+## Getting Started
+
+
+## Demo
+
+See this video:
+
+### Prerequisites
+
+What things you need to install the software and how to install them
